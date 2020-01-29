49b1f49ebf0a010046d2dbe85a35d477c6a432bc
tomsza26 <tomsza27@gmail.com>
2020-01-29 13:18:26 +0100

    Updates

---
+++ precache-manifest.3b94214e9c6cd24554c1668467ffbc99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b47dbb76aaa958fe72c26fcc851b7c",
+    "url": "/fcc-Drum_Machine/index.html"
+  },
+  {
+    "revision": "900d389693c0ebe40bf6",
+    "url": "/fcc-Drum_Machine/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "273d66fd2e7827d34748",
+    "url": "/fcc-Drum_Machine/static/css/main.110c069c.chunk.css"
+  },
+  {
+    "revision": "900d389693c0ebe40bf6",
+    "url": "/fcc-Drum_Machine/static/js/2.e8d99d35.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fcc-Drum_Machine/static/js/2.e8d99d35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "273d66fd2e7827d34748",
+    "url": "/fcc-Drum_Machine/static/js/main.bddd75a2.chunk.js"
+  },
+  {
+    "revision": "b0f8cb27cefca841efe2",
+    "url": "/fcc-Drum_Machine/static/js/runtime-main.fd783714.js"
+  }
+]);
